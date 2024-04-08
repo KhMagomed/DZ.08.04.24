dbc440844a71339e7729f76e351c58e688835d7b
Neto test <xmag91@mail.ru>
2024-04-08 21:37:38 +0300

    New commit

---
+++ src/main/java/ProductManager.java
@@ -0,0 +1,48 @@
+public class ProductManager {
+    private int lastcount;
+    private String[] films = {
+//            "Бладшот",
+//            "Вперед",
+//            "Отель 'Белград'",
+//            "Джентельмены",
+//            "Человек-невидимка",
+//            "Тролли. Мировой тур",
+//            "Номер один"
+    };
+    public ProductManager (int lastcount){
+        this.lastcount = lastcount;
+    }
+    public ProductManager(){
+        this.lastcount=5;
+    }
+    public String[] findLast() {
+        int maxCount;
+        if(films.length < lastcount){
+            maxCount = films.length;
+        } else {
+            maxCount = lastcount;
+        }
+        String[] tmp = new String[maxCount];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = films[films.length - 1 - i];
+        }
+        return tmp;
+
+    }
+
+    public String[] findAll() {
+
+        return films;
+    }
+    public void addFilm(String newFilm){
+        String[] tmp = new String[films.length+1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = newFilm;
+        films = tmp;
+
+    }
+
+
+}
---
+++ src/test/java/ProductManagerTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProductManagerTest {
+
+
+    ProductManager manager = new ProductManager();
+    String[] films = {
+            "Бладшот",
+            "Вперед",
+            "Отель 'Белград'",
+            "Джентельмены",
+            "Человек-невидимка",
+            "Тролли. Мировой тур",
+            "Номер один"
+    };
+    @BeforeEach
+    public void setup (){
+        for (String film : films){
+            manager.addFilm(film);
+        }
+
+    }
+
+    @Test
+    public void addFilmTest(){
+        manager.addFilm("xxxxx");
+        String[] expected = { "Бладшот",
+                "Вперед",
+                "Отель 'Белград'",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли. Мировой тур",
+                "Номер один", "xxxxx"};
+        String[] actual = manager.findAll();
+
+        Assertions.assertArrayEquals(expected,actual);
+
+
+
+    }
+    @Test
+    public void findLastFilm(){
+
+        String[] expected = { "Номер один",
+                "Тролли. Мировой тур",
+                "Человек-невидимка",
+                "Джентельмены",
+                "Отель 'Белград'",
+//                "Вперед","Бладшот"
+        };
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected,actual);
+
+
+
+    }
+
+}
